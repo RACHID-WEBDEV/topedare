fcca21afc7c35f13159b8bbf4912a5ded7145f37
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-09-19 00:21:36 +0100

    Animation  About section

--- src/components/section/AboutIntro.js
+++ src/components/section/AboutIntro.js
@@ -4,6 +4,7 @@ import AboutBanner from "../../assets/about/about-banner.jpg"
 import ShowLove from "../../assets/about/love-shapes.svg"
 import Button from '../form/Button';
 import aboutShape from '../../assets/about/about-shape-3.png';
+import Fade from 'react-reveal/Fade';
 
 
 
@@ -37,40 +38,53 @@ function AboutIntro() {
                         </div>
                         <div className="mt-[-30px] md:mt-0 lg:pb-20 lg:mt-[-100px] xl:mt-[-50px] lg:w-[45%] px-3 ">
                             <div className="md:px-8 md:mt-12 lg:mt-0 lg:px-0">
-                                <p className="text-lg font-black my-2 uppercase text-gray-400">About Us</p>
-                                <h1 className="text-gray-800 mb-6 text-3xl lg:text-4xl ">
-                                    We Can Save More Lifes With Your Helping Hand.
-                                </h1>
-                                <p className=" mb-10 text-gray-600">
-                                    The Tope Dare Foundation is a charity initiative of Babatope Dare with the primary goal of giving back to the society and helping the less privileged and vulnerable in our communities to alleviate human sufferings and create opportunities for a better life
-                                </p>
-                                <div className="flex space-x-4 mb-8 sm:space-x-10">
-                                    <div className="flex space-x-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} className="fill-gray-600 h-8 w-8 shrink-0">
-                                            <path fill="none" d="M0 0h24v24H0z" />
-                                            <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-.997-4L6.76 11.757l1.414-1.414 2.829 2.829 5.656-5.657 1.415 1.414L11.003 16z" />
-                                        </svg>
-                                        <div>
-                                            <span className="font-black text-gray-600 block text-xl dark:text-white">82+</span>
-                                            <span className="text-gray-500">Completed Project</span>
+                                <Fade top>
+                                    <p className="text-lg font-black my-2 uppercase text-gray-400">About Us</p>
+                                </Fade>
+                                <Fade left>
+                                    <h1 className="text-gray-800 mb-6 text-3xl lg:text-4xl ">
+                                        We Can Save More Lifes With Your Helping Hand.
+                                    </h1>
+                                </Fade>
+                                <Fade bottom>
+                                    <p className=" mb-10 text-gray-600">
+                                        The Tope Dare Foundation is a charity initiative of Babatope Dare with the primary goal of giving back to the society and helping the less privileged and vulnerable in our communities to alleviate human sufferings and create opportunities for a better life
+                                    </p>
+
+                                </Fade>
+                                <Fade right>
+                                    <div className="flex space-x-4 mb-8 sm:space-x-10">
+                                        <div className="flex space-x-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} className="fill-gray-600 h-8 w-8 shrink-0">
+                                                <path fill="none" d="M0 0h24v24H0z" />
+                                                <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-.997-4L6.76 11.757l1.414-1.414 2.829 2.829 5.656-5.657 1.415 1.414L11.003 16z" />
+                                            </svg>
+                                            <div>
+                                                <span className="font-black text-gray-600 block text-xl dark:text-white">82+</span>
+                                                <span className="text-gray-500">Completed Project</span>
+                                            </div>
+                                        </div>
+                                        <div className="flex space-x-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} className="fill-gray-600 h-8 w-8 shrink-0">
+                                                <path fill="none" d="M0 0h24v24H0z" />
+                                                <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-.997-4L6.76 11.757l1.414-1.414 2.829 2.829 5.656-5.657 1.415 1.414L11.003 16z" />
+                                            </svg>
+                                            <div>
+                                                <span className="font-black text-gray-600 block text-xl dark:text-white">90.7%</span>
+                                                <span className="text-gray-500">Satisfaction Rate</span>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div className="flex space-x-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} className="fill-gray-600 h-8 w-8 shrink-0">
-                                            <path fill="none" d="M0 0h24v24H0z" />
-                                            <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-.997-4L6.76 11.757l1.414-1.414 2.829 2.829 5.656-5.657 1.415 1.414L11.003 16z" />
-                                        </svg>
-                                        <div>
-                                            <span className="font-black text-gray-600 block text-xl dark:text-white">90.7%</span>
-                                            <span className="text-gray-500">Satisfaction Rate</span>
-                                        </div>
+
+                                </Fade>
+                                <Fade bottom delay={1000}>
+                                    <div className="text-center lg:text-left">
+                                        <Link to="/about">
+                                            <Button className="">Learn More</Button>
+                                        </Link>
                                     </div>
-                                </div>
-                                <div className="text-center lg:text-left">
-                                    <Link to="/about">
-                                        <Button className="">Learn More</Button>
-                                    </Link>
-                                </div>
+
+                                </Fade>
                             </div>
 
                         </div>
