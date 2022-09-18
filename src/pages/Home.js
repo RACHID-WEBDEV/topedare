376b18bf9a4de62f08bd7023e32232c143482ade
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-09-19 00:23:34 +0100

    Animation  About section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AboutIntro from '../components/section/AboutIntro'
-import Hero from '../components/section/Hero'
+// import Hero from '../components/section/Hero'
 import HeroIntro from '../components/section/HeroIntro'
 
 
@@ -9,8 +9,8 @@ const Home = () => {
     <div className="">
       <HeroIntro />
       <AboutIntro />
-      <Hero />
-      <h1 className="text-2xl">Home</h1>
+      {/* <Hero /> */}
+      <h1 className="text-2xl mt-20">Home</h1>
       <p className="text-red-500 font-Fredoka font-normal text-lg">This is a Sub Title</p>
       <p className="text-red-500 mt-5 text-lg">This is a Sub Title</p>
       <p className="text-green-500 font-Fredoka font-medium text-lg">This is a Sub Title</p>
