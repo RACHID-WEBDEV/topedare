be2e93379a55e19e2505829e96646cb197260637
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-09-19 01:15:16 +0100

    create Footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../components/global/Footer'
 import AboutIntro from '../components/section/AboutIntro'
 // import Hero from '../components/section/Hero'
 import HeroIntro from '../components/section/HeroIntro'
@@ -10,15 +11,7 @@ const Home = () => {
       <HeroIntro />
       <AboutIntro />
       {/* <Hero /> */}
-      <h1 className="text-2xl mt-20">Home</h1>
-      <p className="text-red-500 font-Fredoka font-normal text-lg">This is a Sub Title</p>
-      <p className="text-red-500 mt-5 text-lg">This is a Sub Title</p>
-      <p className="text-green-500 font-Fredoka font-medium text-lg">This is a Sub Title</p>
-      <p className="text-gray-300 font-Fredoka font-semibold text-lg">This is a Sub Title</p>
-
-
-
-
+      <Footer />
     </div>
   )
 }
