e50b9eacc1c222274fac4fab354813fc54ce3c59
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-09-21 04:11:03 +0100

    Created Stats Page

--- src/components/section/Stats.js
+++ src/components/section/Stats.js
@@ -2,7 +2,11 @@ import React from "react";
 import SectionTitle from "./SectionTitle";
 import SectionSubTitle from './SectionSubTitle';
 import { Fade } from "react-reveal";
+import mapbg from '../../assets/cta/map-bg.png'
+import map from '../../assets/cta/map.png'
+
 // overflow-hidden object-cover object-[83%]  bg-cover bg-center bg-hero-bg 
+//https://i.ibb.co/KjrPCyW/map.png
 
 export default function Stats() {
     return (
@@ -26,8 +30,8 @@ export default function Stats() {
 
 
                 <div className="md:mt-14 mt-4 relative sm:flex items-center justify-center">
-                    <img src="https://i.ibb.co/KjrPCyW/map.png" alt="world map drawing " className="w-full xl:h-full h-96 object-cover object-fill sm:block hidden" />
-                    <img src="https://i.ibb.co/SXKj9Mf/map-bg.png" alt="mobile world map drawing" className="sm:hidden -mt-10 block w-full h-96 object-cover object-fill absolute z-0" />
+                    <img src={map} alt="world map drawing " className="w-full xl:h-full h-96  object-fill sm:block hidden" />
+                    <img src={mapbg} alt="mobile world map drawing" className="sm:hidden -mt-10 block w-full h-96  object-fill absolute z-0" />
                     <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20  mt-4 left-0 xl:ml-56 sm:ml-12 xl:-mt-40 sm:-mt-12">
                         <p className="text-4xl font-bold text-gray-800">42+</p>
                         <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">FINANCIAL EMPOWERMENT</p>
