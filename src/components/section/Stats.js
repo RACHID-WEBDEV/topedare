f7d01fb81430eb7799eee93aade9a7492dc1d8f2
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-09-22 12:53:33 +0100

    Modified blog intro

--- src/components/section/Stats.js
+++ src/components/section/Stats.js
@@ -29,22 +29,31 @@ export default function Stats() {
                 <div className="md:mt-14 mt-4 relative sm:flex items-center justify-center">
                     <img src={map} alt="world map drawing " className="w-full xl:h-full h-96 object-fill sm:block hidden" />
                     <img src={mapbg} alt="mobile world map drawing" className="sm:hidden -mt-10 block w-full h-96  object-fill absolute z-0" />
-                    <div className="shadow-lg xl:p-6 p-4 w-52 sm:w-auto hidden lg:block lg:right-80 lg:top-[-300px] bg-white sm:absolute relative z-30 mt-4 xl:mt-80 sm:mt-56 xl:-ml-0 sm:-ml-12">
-                        <p className="text-4xl font-bold text-gray-800">88+</p>
-                        <p className="text-base leading-4 uppercase xl:mt-4 mt-2 text-gray-600">Completed Project</p>
-                    </div>
-                    <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20  mt-4 left-0 xl:ml-56 sm:ml-12 xl:-mt-40 sm:-mt-12">
-                        <p className="text-4xl font-bold text-gray-800">42+</p>
-                        <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">FINANCIAL EMPOWERMENT</p>
-                    </div>
-                    <div className="shadow-lg xl:p-6 p-4 w-52 sm:w-auto  bg-white sm:absolute relative z-20 mt-4 xl:mt-80 sm:mt-56 xl:-ml-0 sm:-ml-12">
-                        <p className="text-4xl font-bold text-gray-800">18+</p>
-                        <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">SCHOOLS SUPPORTED</p>
-                    </div>
-                    <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 md:mt-0 sm:-mt-5 mt-4 right-0 xl:mr-56 sm:mr-24">
-                        <p className="text-4xl font-bold text-gray-800">320+</p>
-                        <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">HAPPY CHILDREN</p>
-                    </div>
+                    <Fade top>
+                        <div className="shadow-lg xl:p-6 p-4 w-52 sm:w-auto hidden lg:block lg:right-80 lg:top-[-300px] bg-white sm:absolute relative z-30 mt-4 xl:mt-80 sm:mt-56 xl:-ml-0 sm:-ml-12">
+                            <p className="text-4xl font-bold text-gray-800">88+</p>
+                            <p className="text-base leading-4 uppercase xl:mt-4 mt-2 text-gray-600">Completed Project</p>
+                        </div>
+                    </Fade>
+                    <Fade left>
+                        <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20  mt-4 left-0 xl:ml-56 sm:ml-12 xl:-mt-40 sm:-mt-12">
+                            <p className="text-4xl font-bold text-gray-800">42+</p>
+                            <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">FINANCIAL EMPOWERMENT</p>
+                        </div>
+
+                    </Fade>
+                    <Fade bottom>
+                        <div className="shadow-lg xl:p-6 p-4 w-52 sm:w-auto  bg-white sm:absolute relative z-20 mt-4 xl:mt-80 sm:mt-56 xl:-ml-0 sm:-ml-12">
+                            <p className="text-4xl font-bold text-gray-800">18+</p>
+                            <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">SCHOOLS SUPPORTED</p>
+                        </div>
+                    </Fade>
+                    <Fade right>
+                        <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 md:mt-0 sm:-mt-5 mt-4 right-0 xl:mr-56 sm:mr-24">
+                            <p className="text-4xl font-bold text-gray-800">320+</p>
+                            <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">HAPPY CHILDREN</p>
+                        </div>
+                    </Fade>
                 </div>
             </div>
         </div>
