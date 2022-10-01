502b2d63c56a00e50732c08f757d304b848551cc
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-10-01 14:53:55 +0100

    Modified Blogs data

--- src/components/section/BlogIntro.js
+++ src/components/section/BlogIntro.js
@@ -1,9 +1,48 @@
 import React from 'react'
-import BlogImg from "../../assets/images/gallery-v1-3.jpg"
 import { Link } from 'react-router-dom';
 import SectionSubTitle from './SectionSubTitle';
 import Button from '../form/Button';
 import { Fade } from 'react-reveal';
+import blogdata from '../../data/blog';
+
+export const LargeBlogcard = ({ blogPhoto, category, short_desc, title, slug }) => (
+    <div className="lg:w-6/12 group">
+        <Link to={slug}>
+            <div className="relative overflow-hidden rounded-md w-full max-h-96 h-80 ">
+                <img className="transform group-hover:scale-110 w-full h-full rounded-xl mb-6 transition-transform duration-500" src={blogPhoto} alt="blog" />
+                <span className="bg-yellow-300 absolute bottom-2 left-2 text-darken text-xs font-semibold px-4 py-1 uppercase  rounded-full">{category}</span>
+            </div>
+        </Link>
+        <Link to={slug}>
+            <h1 className="text-gray-900 font-bold my-3 lg:mt-4 font-DmSans text-xl lg:text-2xl two-row-paragraph">{title}</h1>
+
+        </Link>
+        <p className="text-gray-600 text-base lg:text-xl mb-3  two-row-paragraph">{short_desc}</p>
+        <Link to={slug} className="underline">Read more</Link>
+    </div>
+)
+
+export const SmallBlogcard = ({ blogPhoto, category, short_desc, title, slug }) => (
+    <div className="flex space-x-5">
+        <div className="w-6/12 lg:w-4/12">
+            <div className="relative group">
+                <Link to={slug}>
+                    <div className="relative overflow-hidden rounded-md max-w-[200px] max-h-32 lg:max-h-40 ">
+                        <img className="transform group-hover:scale-110 w-full h-full rounded-xl mb-6 transition-transform duration-500" src={blogPhoto} alt="blog" />
+                        <span className="bg-yellow-300 absolute bottom-2 right-2 text-darken text-xs font-semibold px-4 py-1 uppercase  rounded-full">{category}</span>
+                    </div>
+                </Link>
+
+            </div>
+        </div>
+        <div className="w-8/12">
+            <Link to={slug}>
+                <h1 className="text-gray-800 font-DmSans text-xl font-bold two-row-paragraph">{title}</h1>
+            </Link>
+            <p className="text-gray-500 my-1 text-sm lg:text-lg three-row-paragraph">{short_desc}</p>
+        </div>
+    </div>
+)
 
 const BlogIntro = () => {
     return (
@@ -17,50 +56,21 @@ const BlogIntro = () => {
                     coming from the blog</p>
             </Fade>
             <div className="p-4 lg:p-12 flex flex-col lg:flex-row lg:space-x-20 ">
-                <div className="lg:w-6/12">
-                    <img className="w-full h-72 rounded-xl mb-6" alt="blog" src={BlogImg} />
-                    <span className="bg-yellow-300 text-darken font-semibold px-4 py-px text-sm rounded-full">NEWS</span>
-                    <h1 className="text-gray-800 font-semibold my-3 font-DmSans text-xl two-row-paragraph">Rise of Global Charity in Modern World</h1>
-                    <p className="text-gray-500 text-base mb-3 two-row-paragraph">Nostrud tem exrcitation duis laboris nisi ut aliquip sed duis aute cupidata con proident sunt culpa....</p>
-                    <Link to="" className="underline">Read more</Link>
-                </div>
+                {
+                    blogdata.slice(0, 1).map(({ blogImg, category, short_desc, title, url }, idx) => (
+                        <LargeBlogcard key={idx} blogPhoto={blogImg} category={category} slug={url}
+                            short_desc={short_desc} title={title} />
+                    ))
+                }
+
                 <div className="lg:w-7/12 flex flex-col justify-between mt-12 space-y-5 lg:space-y-0 lg:mt-0">
-                    <div className="flex space-x-5">
-                        <div className="w-4/12">
-                            <div className="relative">
-                                <img className="rounded-xl h-28 lg:h-36  w-full" alt="blog" src={BlogImg} />
-                                <span className="absolute bottom-2 right-2 bg-yellow-300 text-darken font-semibold px-4 py-px text-sm rounded-full hidden sm:block">PRESS RELEASE</span>
-                            </div>
-                        </div>
-                        <div className="w-8/12">
-                            <h1 className="text-gray-800 font-DmSans text-xl font-bold two-row-paragraph">How to become a Good Volunteer Today</h1>
-                            <p className="text-gray-500 my-2 sm:my-4 text-base two-row-paragraph">Nostrud tem exrcitation duis laboris nisi ut aliquip sed duis aute cupidata con proident sunt culpa....</p>
-                        </div>
-                    </div>
-                    <div className="flex space-x-5">
-                        <div className="w-4/12">
-                            <div className="relative">
-                                <img className="rounded-xl h-28 lg:h-36 w-full" alt="blog" src={BlogImg} />
-                                <span className="absolute bottom-2 right-2 bg-yellow-300 text-darken font-semibold px-4 py-px text-sm rounded-full hidden sm:block">NEWS</span>
-                            </div>
-                        </div>
-                        <div className="w-8/12">
-                            <h1 className="text-gray-800 font-DmSans text-xl font-bold two-row-paragraph">Empower the Dropout Innocents is Key</h1>
-                            <p className="text-gray-500 my-2 sm:my-4 text-base two-row-paragraph">Zoom was never created to be a consumer product. Nonetheless, the...</p>
-                        </div>
-                    </div>
-                    <div className="flex space-x-5">
-                        <div className="w-4/12">
-                            <div className="relative">
-                                <img className="rounded-xl h-28 lg:h-36 w-full" alt="blog" src={BlogImg} />
-                                <span className="absolute bottom-2 right-2 bg-yellow-300 text-darken font-semibold px-4 py-px text-sm rounded-full hidden sm:block">NEWS</span>
-                            </div>
-                        </div>
-                        <div className="w-8/12">
-                            <h1 className="text-gray-800 font-DmSans text-xl font-bold two-row-paragraph">Donation is Hope for Poor Childrens in Africa</h1>
-                            <p className="text-gray-500 my-2 sm:my-4 text-base two-row-paragraph">This year, investors have reaped big financial returns from betting on Zoom...</p>
-                        </div>
-                    </div>
+                    {
+                        blogdata.slice(1, 4).map(({ blogImg, category, short_desc, title, url }, idx) => (
+                            <SmallBlogcard key={idx} blogPhoto={blogImg} category={category} slug={url}
+                                short_desc={short_desc} title={title} />
+                        ))
+                    }
+
                 </div>
             </div>
             <Link to="blog">
