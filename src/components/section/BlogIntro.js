ed9e3cf799c9935d0aed309a6661014e07e349c0
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-10-03 22:16:31 +0100

    Added Slug to BlogIntro section

--- src/components/section/BlogIntro.js
+++ src/components/section/BlogIntro.js
@@ -4,21 +4,22 @@ import SectionSubTitle from './SectionSubTitle';
 import Button from '../form/Button';
 import { Fade } from 'react-reveal';
 import blogdata from '../../data/blog';
+import { kebabCase } from '../../utils/utils'
 
 export const LargeBlogcard = ({ blogPhoto, category, id, short_desc, title, slug }) => (
     <div className="lg:w-6/12 group">
-        <Link to={`/blog/${id}`}>
+        <Link to={`/blog/${kebabCase(title)}`}>
             <div className="relative overflow-hidden rounded-md w-full max-h-96 h-80 ">
                 <img className="transform group-hover:scale-110 w-full h-full rounded-xl mb-6 transition-transform duration-500" src={blogPhoto} alt="blog" />
                 <span className="bg-yellow-300 absolute bottom-2 left-2 text-darken text-xs font-semibold px-4 py-1 uppercase  rounded-full">{category}</span>
             </div>
         </Link>
-        <Link to={`/blog/${id}`}>
+        <Link to={`/blog/${kebabCase(title)}`}>
             <h1 className="text-gray-900 font-bold my-3 lg:mt-4 font-DmSans text-xl lg:text-2xl two-row-paragraph">{title}</h1>
 
         </Link>
         <p className="text-gray-600 text-base lg:text-xl mb-3  two-row-paragraph">{short_desc}</p>
-        <Link to={`/blog/${id}`} className="underline">Read more</Link>
+        <Link to={`/blog/${kebabCase(title)}`} className="underline">Read more</Link>
     </div>
 )
 
@@ -27,7 +28,7 @@ export const SmallBlogcard = ({ blogPhoto, category, id, short_desc, title, slug
         <div className="flex space-x-5 ">
             <div className="w-6/12 lg:w-4/12">
                 <div className="relative group">
-                    <Link to={`/blog/${id}`}>
+                    <Link to={`/blog/${kebabCase(title)}`}>
                         <div className="relative overflow-hidden rounded-md max-w-[200px] max-h-32 lg:max-h-40 ">
                             <img className="transform group-hover:scale-110 w-full h-full rounded-xl mb-6 transition-transform duration-500" src={blogPhoto} alt="blog" />
                             <span className="bg-yellow-300 absolute bottom-2 right-2 text-darken text-xs font-semibold px-4 py-1 uppercase  rounded-full">{category}</span>
@@ -37,7 +38,7 @@ export const SmallBlogcard = ({ blogPhoto, category, id, short_desc, title, slug
                 </div>
             </div>
             <div className="w-8/12">
-                <Link to={`/blog/${id}`}>
+                <Link to={`/blog/${kebabCase(title)}`}>
                     <h1 className="text-gray-800 font-DmSans text-xl font-bold two-row-paragraph">{title}</h1>
                 </Link>
                 <p className="text-gray-500 my-1 text-sm lg:text-lg three-row-paragraph">{short_desc}</p>
