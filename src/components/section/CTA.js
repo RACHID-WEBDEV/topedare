abfe4812946235f527a849597f4c2f309d07b056
github.com/RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2022-10-12 22:50:09 +0100

    Modified CTA Componets

--- src/components/section/CTA.js
+++ src/components/section/CTA.js
@@ -3,74 +3,77 @@ import ShowLove from "../../assets/about/love-shapes.svg"
 import Button from '../form/Button'
 import { Link } from 'react-router-dom'
 import { Fade } from 'react-reveal'
-import SectionTitle from './SectionTitle'
-import SectionSubTitle from './SectionSubTitle';
+// import SectionTitle from './SectionTitle'
+// import SectionSubTitle from './SectionSubTitle';
+import useSWR from 'swr'
+import parse from 'html-react-parser';
 
 
 const CTA = ({ ctaSectionHeading, title, subTitle, qoute, qouteAuthor, volunteerHeading, volunteerTitle }) => {
+
+    const { data, error } = useSWR(`${process.env.REACT_APP_BASE_URL}public/content/components?user=${process.env.REACT_APP_USER_lOGIN_ID}`)
+
+    if (error) return console.log(error)
+
+    if (!data) {
+        return <h1>Loading...</h1>
+    }
+    const renderIntro = data?.data?.components?.find(({ slug }) => {
+        return slug === "cta-intro"
+    })
+    const renderQoute = data?.data?.components?.find(({ slug }) => {
+        return slug === "cta-qoute"
+    })
+    const renderVolunteer = data?.data?.components?.find(({ slug }) => {
+        return slug === "cta-be-a-volunteer"
+    })
+
     return (
         <div className="py-5 mb-10 rounded-md  ">
-            <div className="px-5 lg:px-6 lg:pl-8 xl:pl-20 xl:pr-8">
-                <div className="relative">
-                    <Fade top>
-                        <SectionTitle title={ctaSectionHeading} />
-                    </Fade>
-                    <Fade left>
-                        <div className="max-w-2xl">
-                            <SectionSubTitle subTitle={title} />
-                            <p className=" mb-10 text-gray-600">
-                                {subTitle}
-                            </p>
-                        </div>
-                    </Fade>
-
-                </div>
-            </div>
+            {renderIntro &&
+                <div className="px-5 lg:px-6 lg:pl-8 xl:pl-20 xl:pr-8">
+                    <div className="relative">
+                        <Fade top>
+                            {parse(renderIntro?.description)}
+                        </Fade>
+                    </div>
+                </div>}
             <section className="cta-area relative block overflow-hidden">
                 <div className="cta-area_bg bg-cta-bg2 absolute left-0 bottom-0 right-0 bg-scroll bg-no-repeat bg-cover" />
                 <div className="w-full">
                     <div className="">
                         <div className="">
                             <div className="px-3 lg:px-20 relative block  pb-20">
-                                <div className="cta-content_top pl-4 lg:px-[100px] pt-[4.5rem] pb-[4.8125rem] bg-cta-bg relative block bg-scroll bg-no-repeat bg-cover " >
-                                    <Fade top>
-                                        <h2 className="text-white w-[19.5625rem] lg:w-[21.25rem] text-[1.625rem] lg:text-4xl font-medium mb-6 leading-[2.75rem]">{qoute}</h2>
-
-                                    </Fade>
+                                {renderQoute && <div className="cta-content_top pl-4 lg:px-[100px] pt-[4.5rem] pb-[4.8125rem] bg-cta-bg relative block bg-scroll bg-no-repeat bg-cover " >
                                     <Fade bottom>
-                                        <div className="name relative block pl-16">
-                                            <p className="text-white text-lg lg:text-xl leading-4 font-normal">{qouteAuthor}</p>
-                                        </div>
+                                        {parse(renderQoute?.description)}
                                     </Fade>
-                                </div>
+                                </div>}
 
-                                <div className="relative flex flex-row items-center justify-between flex-wrap mt-10 lg:mt-20">
-                                    <div className="relative flex mb-10 lg:mb-0 items-center ">
-                                        <Fade left>
-                                            <div className="relative block">
-                                                <img src={ShowLove} alt="icon" className="w-16 h-16 lg:w-20 lg:h-20" />
-                                            </div>
-                                        </Fade>
-                                        <Fade bottom>
-                                            <div className="relative block pl-5">
-                                                <p className="text-gray-600 text-lg lg:text-2xl leading-6 font-medium m-0 mb-2">{volunteerHeading}</p>
-                                                <h2 className="text-2xl lg:text-5xl font-extrabold text-tdf-blue-50/80 ">{volunteerTitle}</h2>
-                                            </div>
+                                {renderVolunteer &&
+                                    <div className="relative flex flex-row items-center justify-between flex-wrap mt-10 lg:mt-20">
+                                        <div className="relative flex mb-10 lg:mb-0 items-center ">
+                                            <Fade left>
+                                                <div className="relative block">
+                                                    <img src={ShowLove} alt="icon" className="w-16 h-16 lg:w-20 lg:h-20" />
+                                                </div>
+                                            </Fade>
+                                            <Fade bottom>
+                                                {parse(renderVolunteer?.description)}
+                                            </Fade>
+                                        </div>
+                                        <div className="relative mx-auto lg:mx-0 block">
+                                            <Fade right>
+                                                <Link to="/contact">
+                                                    <Button>
+                                                        Register
+                                                        Now
+                                                    </Button>
+                                                </Link>
+                                            </Fade>
 
-                                        </Fade>
-                                    </div>
-                                    <div className="relative mx-auto lg:mx-0 block">
-                                        <Fade right>
-                                            <Link to="/contact">
-                                                <Button>
-                                                    Register
-                                                    Now
-                                                </Button>
-                                            </Link>
-                                        </Fade>
-
-                                    </div>
-                                </div>
+                                        </div>
+                                    </div>}
 
                             </div>
                         </div>
